1c0fa3dc0c6ccb295f6d484cb69ff84e7780b4de
bouzuya <m@bouzuya.net>
2018-04-13 19:41:20 +0900

    add User

--- src/Main.purs
+++ src/Main.purs
@@ -3,6 +3,7 @@ module Main (main) where
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Functor ((<$>))
+import Data.Semigroup ((<>))
 import Data.Show (class Show)
 import Data.UUID (GENUUID, UUID, genUUID)
 import Prelude (Unit, bind, discard, show, ($))
@@ -10,8 +11,14 @@ import Prelude (Unit, bind, discard, show, ($))
 newtype UserId = UserId UUID
 derive newtype instance showUserId :: Show UserId
 
+newtype User = User { id :: UserId, name :: String }
+instance showUser :: Show User where
+  show (User { id, name }) =
+    "User { id: \"" <> show id <> "\", name: \"" <> name <> "\" }"
+
 main :: forall e. Eff (console :: CONSOLE, uuid :: GENUUID | e) Unit
 main = do
   userId <- UserId <$> genUUID
-  log $ show userId
+  let user = User { id: userId, name: "bouzuya" }
+  log $ show user
   log "Hello sailor!"
