b2c8d8c1a92a8d205f252e813b95ee17a1fcb433
bouzuya <m@bouzuya.net>
2018-04-13 19:22:32 +0900

    use uuid

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,12 @@
 module Main (main) where
 
-import Prelude (Unit)
+import Prelude (Unit, bind, discard, show, ($))
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Data.UUID (GENUUID, genUUID)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+main :: forall e. Eff (console :: CONSOLE, uuid :: GENUUID | e) Unit
 main = do
+  uuid' <- genUUID
+  log $ show uuid'
   log "Hello sailor!"
