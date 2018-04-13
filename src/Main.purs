683567a385b143e5b1effdf4eb6b92157ef50848
bouzuya <m@bouzuya.net>
2018-04-13 19:29:22 +0900

    add UserId

--- src/Main.purs
+++ src/Main.purs
@@ -1,12 +1,17 @@
 module Main (main) where
 
-import Prelude (Unit, bind, discard, show, ($))
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Data.UUID (GENUUID, genUUID)
+import Data.Functor ((<$>))
+import Data.Show (class Show)
+import Data.UUID (GENUUID, UUID, genUUID)
+import Prelude (Unit, bind, discard, show, ($))
+
+newtype UserId = UserId UUID
+derive newtype instance showUserId :: Show UserId
 
 main :: forall e. Eff (console :: CONSOLE, uuid :: GENUUID | e) Unit
 main = do
-  uuid' <- genUUID
-  log $ show uuid'
+  userId <- UserId <$> genUUID
+  log $ show userId
   log "Hello sailor!"
