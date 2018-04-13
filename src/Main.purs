e98a76ec8b55660e19b6039d3d51147828e12dbf
bouzuya <m@bouzuya.net>
2018-04-13 20:20:26 +0900

    add toIso8601

--- src/Main.purs
+++ src/Main.purs
@@ -2,13 +2,16 @@ module Main (main) where
 
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Data.DateTime.Instant (instant)
+import Data.DateTime.Instant (Instant, instant, toDateTime)
+import Data.Formatter.DateTime (FormatterCommand(..), format)
 import Data.Functor ((<$>))
+import Data.List (fromFoldable)
+import Data.Maybe (Maybe(..), fromJust)
 import Data.Semigroup ((<>))
 import Data.Show (class Show)
-import Data.Time.Duration (Milliseconds (Milliseconds))
+import Data.Time.Duration (Milliseconds(Milliseconds)) as DTD
 import Data.UUID (GENUUID, UUID, genUUID)
-import Prelude (Unit, bind, discard, show, ($))
+import Prelude (Unit, bind, discard, pure, show, ($))
 
 newtype UserId = UserId UUID
 derive newtype instance showUserId :: Show UserId
@@ -21,6 +24,26 @@ instance showUser :: Show User where
 newtype URL = URL String
 derive newtype instance showURL :: Show URL
 
+toIso8601 :: Instant -> String
+toIso8601 i = format formatter $ toDateTime i
+  where
+    dateFormat =
+      [ YearFull
+      , Placeholder "-"
+      , MonthTwoDigits
+      , Placeholder "-"
+      , DayOfMonthTwoDigits
+      ]
+    dateTimeFormat = dateFormat <> [Placeholder "T"] <> timeFormat
+    timeFormat =
+      [ Hours24
+      , Placeholder ":"
+      , MinutesTwoDigits
+      , Placeholder ":"
+      , SecondsTwoDigits
+      ]
+    formatter = fromFoldable $ dateTimeFormat <> [Placeholder "Z"]
+
 main :: forall e. Eff (console :: CONSOLE, uuid :: GENUUID | e) Unit
 main = do
   userId <- UserId <$> genUUID
@@ -28,5 +51,8 @@ main = do
   log $ show user
   let url = URL "https://bouzuya.net/"
   log $ show url
-  log $ show $ instant $ Milliseconds 1523616518.0
+  let instant' = instant $ DTD.Milliseconds 1523616518300.0
+  log $ case instant' of
+    Nothing -> ""
+    Just instant'' -> toIso8601 instant''
   log "Hello sailor!"
