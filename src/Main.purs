8c5be282cd8bc8f04f46235a334ea46456642bd1
bouzuya <m@bouzuya.net>
2018-04-13 20:54:43 +0900

    use now

--- src/Main.purs
+++ src/Main.purs
@@ -2,26 +2,26 @@ module Main (main) where
 
 import Control.Applicative (pure)
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Now (NOW, now)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Data.DateTime.Instant (instant)
 import Data.FaceWithTime (fwt, url)
 import Data.Function (($))
 import Data.Functor ((<$>))
 import Data.Show (show)
-import Data.Time.Duration (Milliseconds(Milliseconds)) as DTD
 import Data.UUID (GENUUID, genUUID)
 import Data.User (user)
 import Data.UserId (userId)
 import Prelude (Unit, bind, discard)
 
-main :: forall e. Eff (console :: CONSOLE, uuid :: GENUUID | e) Unit
+main :: forall e. Eff (console :: CONSOLE, now :: NOW, uuid :: GENUUID | e) Unit
 main = do
   userId' <- userId <$> genUUID
   let user' = user { id: userId', name: "bouzuya" }
   log $ show user'
+  instant' <- now
   let fwt' = do
         face <- url "https://bouzuya.net/"
-        time <- instant $ DTD.Milliseconds 1523616518300.0
+        time <- pure $ instant'
         pure $ fwt { face, time }
   log $ show fwt'
   log "Hello sailor!"
