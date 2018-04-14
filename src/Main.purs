2c858f795250662822c6d0a1ac7db33b17fe02e0
bouzuya <m@bouzuya.net>
2018-04-14 15:22:01 +0900

    check route

--- src/Main.purs
+++ src/Main.purs
@@ -1,24 +1,26 @@
 module Main (main) where
 
 import Control.Applicative (pure)
-import Control.IxMonad ((:*>))
+import Control.IxMonad (ibind)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Now (NOW, now)
+import Data.Either (Either(..))
 import Data.FaceWithTime (fwt, url)
 import Data.Function (($))
 import Data.Functor ((<$>))
 import Data.Show (show)
 import Data.UUID (GENUUID, UUID, genUUID)
 import Data.User (User, user)
-import Routing (match)
-import Route (myRoute)
 import Data.UserId (userId)
 import Hyper.Node.Server (defaultOptionsWithLogging, runServer)
+import Hyper.Request (getRequestData)
 import Hyper.Response (closeHeaders, respond, writeStatus)
 import Hyper.Status (statusOK)
 import Node.HTTP (HTTP)
 import Prelude (Unit, bind, discard)
+import Route (myRoute)
+import Routing (match)
 
 newUser :: String -> UUID -> User
 newUser name uuid = user { id: userId uuid, name }
@@ -43,7 +45,13 @@ main = do
   log $ show $ match myRoute "/users"
   log $ show $ match myRoute "/users/abc"
   log "Hello sailor!"
-  let app = writeStatus statusOK
-            :*> closeHeaders
-            :*> respond "Hello, Hyper!"
+  let app = do
+        request <- getRequestData
+        _ <- writeStatus statusOK
+        _ <- closeHeaders
+        case match myRoute request.url of
+          (Left _) -> respond "ERROR"
+          (Right route) -> respond $ show $ route
+        where
+          bind = ibind
   runServer defaultOptionsWithLogging {} app
