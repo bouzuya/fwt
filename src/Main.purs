8c9d8b7875a448433599d3f04a8bb6a230467ee4
bouzuya <m@bouzuya.net>
2018-04-15 18:22:14 +0900

    add ref

--- src/Main.purs
+++ src/Main.purs
@@ -3,9 +3,13 @@ module Main (main) where
 import Control.Applicative (pure)
 import Control.IxMonad ((:*>), (:>>=))
 import Control.Monad.Aff (Aff)
+import Control.Monad.Aff.Class (liftAff)
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Now (NOW, now)
+import Control.Monad.Eff.Ref (REF, Ref, newRef, readRef)
+import Control.Monad.State (get, modify, put, runState)
 import Data.Argonaut (class EncodeJson, encodeJson, fromArray, fromObject, jsonNull, stringify)
 import Data.Either (either)
 import Data.FaceWithTime (FaceWithTime, fwt)
@@ -20,7 +24,7 @@ import Data.UUID (GENUUID, UUID, genUUID)
 import Data.User (User, user)
 import Data.UserId (userId)
 import Hyper.Conn (Conn)
-import Hyper.Middleware (Middleware)
+import Hyper.Middleware (Middleware, lift')
 import Hyper.Node.Server (HttpRequest, HttpResponse, defaultOptionsWithLogging, runServer)
 import Hyper.Request (RequestData, getRequestData)
 import Hyper.Response (ResponseEnded, StatusLineOpen, closeHeaders, respond, writeStatus)
@@ -32,6 +36,7 @@ import Route (Action(..), route)
 newUser :: String -> UUID -> User
 newUser name uuid = user { id: userId uuid, name }
 
+type State = { users :: Array { user :: User, fwt :: Maybe FaceWithTime } }
 newtype UsersView =
   UsersView (Array { user :: User, fwt :: Maybe FaceWithTime })
 
@@ -44,13 +49,15 @@ instance encodeJsonUsersView :: EncodeJson UsersView where
         ]
 
 view
-  :: Array { user :: User, fwt :: Maybe FaceWithTime }
+  :: forall e. Ref State
   -> Maybe Action
-  -> Tuple Status String
-view _ (Just GetIndex) = Tuple statusOK "{\"status\":\"OK\"}" -- TODO: HTML
-view users (Just GetUsers) = Tuple statusOK $ stringify $ encodeJson $ UsersView users
-view _ (Just (UpdateUser _)) = Tuple statusOK "{\"status\":\"OK\"}"
-view _ _ = Tuple statusNotFound "{\"status\":\"Error\"}"
+  -> Eff ( ref :: REF | e ) (Tuple Status String)
+view _ (Just GetIndex) = pure $ Tuple statusOK "{\"status\":\"OK\"}" -- TODO: HTML
+view ref (Just GetUsers) = do
+  { users } <- readRef ref
+  pure $ Tuple statusOK $ stringify $ encodeJson $ UsersView users
+view _ (Just (UpdateUser _)) = pure $ Tuple statusOK "{\"status\":\"OK\"}"
+view _ _ = pure $ Tuple statusNotFound "{\"status\":\"Error\"}"
 
 action :: RequestData -> Maybe Action
 action request = do
@@ -59,19 +66,22 @@ action request = do
   route method path
 
 app
-  :: forall e. Array { user :: User, fwt :: Maybe FaceWithTime }
+  :: forall e. Ref State
   -> Middleware
-      (Aff ( http ∷ HTTP | e ) )
+      (Aff ( http ∷ HTTP, ref :: REF | e ) )
       (Conn HttpRequest (HttpResponse StatusLineOpen) {})
       (Conn HttpRequest (HttpResponse ResponseEnded) {})
       Unit
-app users = getRequestData :>>= \request ->
-  let (Tuple status body) = view users $ action request
-  in writeStatus status :*> closeHeaders :*> respond body
+app ref =
+  getRequestData
+    :>>= \request -> (lift' $ liftEff $ view ref $ action request)
+    :>>= \(Tuple status body) ->
+      writeStatus status :*> closeHeaders :*> respond body
 
 main :: forall e. Eff ( console :: CONSOLE
                       , http :: HTTP
                       , now :: NOW
+                      , ref :: REF
                       , uuid :: GENUUID
                       | e
                       )
@@ -89,4 +99,5 @@ main = do
   -- TODO: test route
   -- TODO: extract app handler
   let users = [{ user: user', fwt: fwt' }]
-  runServer defaultOptionsWithLogging {} $ app users
+  ref <- newRef { users }
+  runServer defaultOptionsWithLogging {} $ app ref
