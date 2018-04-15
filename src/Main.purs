e4454439c35c1c8d5f2292e8e2b176510d4f4a21
bouzuya <m@bouzuya.net>
2018-04-15 09:45:08 +0900

    add EncodeJson UsersView

--- src/Main.purs
+++ src/Main.purs
@@ -5,7 +5,7 @@ import Control.IxMonad (ibind)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Now (NOW, now)
-import Data.Argonaut (encodeJson, stringify)
+import Data.Argonaut (class EncodeJson, encodeJson, fromArray, fromObject, jsonNull, stringify)
 import Data.Either (Either(..))
 import Data.FaceWithTime (FaceWithTime, fwt)
 import Data.Function (($))
@@ -14,6 +14,8 @@ import Data.List (fold)
 import Data.Maybe (Maybe, maybe)
 import Data.Semigroup ((<>))
 import Data.Show (show)
+import Data.StrMap (fromFoldable) as StrMap
+import Data.Tuple (Tuple(..))
 import Data.URL (url)
 import Data.UUID (GENUUID, UUID, genUUID)
 import Data.User (User, user)
@@ -30,15 +32,19 @@ import Routing (match)
 newUser :: String -> UUID -> User
 newUser name uuid = user { id: userId uuid, name }
 
-usersView :: Array { user :: User, fwt :: Maybe FaceWithTime } -> String
-usersView xs = "[" <> (fold $ jsonX <$> xs) <> "]"
-  where
-    jsonX ({ user, fwt })
-      =  "{\"user\":"
-      <> (stringify $ encodeJson user)
-      <> ",\"fwt\":"
-      <> (maybe "null" (\x -> stringify $ encodeJson x) fwt)
-      <> "}"
+newtype UsersView =
+  UsersView (Array { user :: User, fwt :: Maybe FaceWithTime })
+
+instance encodeJsonUsersView :: EncodeJson UsersView where
+  encodeJson (UsersView xs) = fromArray $ encodeJson' <$> xs
+    where
+      encodeJson' ({ user, fwt }) = fromObject $ StrMap.fromFoldable
+        [ Tuple "user" $ encodeJson user
+        , Tuple "fwt" $ maybe jsonNull encodeJson fwt
+        ]
+
+usersView :: UsersView -> String
+usersView v = stringify $ encodeJson v
 
 main :: forall e. Eff ( console :: CONSOLE
                       , http :: HTTP
@@ -70,7 +76,7 @@ main = do
         case match myRoute request.url of
           (Left _) -> respond "ERROR"
           (Right RouteIndex) -> respond "OK" -- TODO: HTML
-          (Right RouteUsers) -> respond $ usersView users -- TODO: JSON
+          (Right RouteUsers) -> respond $ usersView $ UsersView users
           (Right (RouteUser id)) -> respond $ show user' -- TODO: JSON
         where
           bind = ibind
