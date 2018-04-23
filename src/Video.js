b3b3db6ca55b91bbd9e22d7977881a6311e7f5ad
bouzuya <m@bouzuya.net>
2018-04-23 22:43:55 +0900

    add MediaStream to Video

--- src/Video.js
+++ src/Video.js
@@ -11,6 +11,22 @@ exports.getVideoElementByIdImpl = function (id, Just, Nothing) {
   };
 };
 
+exports.getMediaStream = function () {
+  return navigator
+    .mediaDevices
+    .getUserMedia({ audio: false, video: true });
+};
+
+exports.getSourceObjectImpl = function (e, Just, Nothing) {
+  return function () {
+    if (e.srcObject === null) {
+      return Nothing;
+    } else {
+      return Just(e.srcObject);
+    }
+  };
+};
+
 exports.getVideoHeight = function (e) {
   return function () {
     return e.videoHeight;
@@ -20,6 +36,14 @@ exports.getVideoHeight = function (e) {
 exports.getVideoWidth = function (e) {
   return function () {
     return e.videoWidth;
+  };
+};
+
+exports.setSourceObject = function (srcObject) {
+  return function (e) {
+    return function () {
+      e.srcObject = srcObject;
+    };
   };
 };
 
--- src/Video.purs
+++ src/Video.purs
@@ -1,20 +1,40 @@
 module Video
-  ( VIDEO
+  ( MEDIA
+  , VIDEO
+  , MediaStream
   , VideoElement
+  , getMediaStream
+  , getSourceObject
   , getVideoElementById
   , getVideoHeight
   , getVideoWidth
+  , setSourceObject
   , videoElementToCanvasImageSource
   ) where
 
 import Control.Monad.Eff (Eff, kind Effect)
+import Control.Promise (Promise)
 import Data.Function.Uncurried (Fn3, runFn3)
 import Data.Maybe (Maybe(..))
+import Data.Unit (Unit)
 import Graphics.Canvas (CanvasImageSource)
 
+foreign import data MEDIA :: Effect
 foreign import data VIDEO :: Effect
 
+foreign import data MediaStream :: Type
 foreign import data VideoElement :: Type
+
+foreign import getMediaStream
+  :: forall e. Eff ( media :: MEDIA | e ) (Promise MediaStream)
+
+foreign import getSourceObjectImpl
+  :: forall r e
+  . Fn3
+      VideoElement
+      (MediaStream -> r)
+      r
+      (Eff (video :: VIDEO | e) r)
 
 foreign import getVideoElementByIdImpl
   :: forall r e
@@ -26,9 +46,18 @@ foreign import getVideoElementByIdImpl
 
 foreign import getVideoHeight
   :: forall e. VideoElement -> Eff (video :: VIDEO | e) Number
+
 foreign import getVideoWidth
   :: forall e. VideoElement -> Eff (video :: VIDEO | e) Number
+
+foreign import setSourceObject
+  :: forall e. MediaStream -> VideoElement -> Eff (video :: VIDEO | e) Unit
+
 foreign import videoElementToCanvasImageSource :: VideoElement -> CanvasImageSource
+
+getSourceObject
+  :: forall e. VideoElement -> Eff (video :: VIDEO | e) (Maybe MediaStream)
+getSourceObject e = runFn3 getSourceObjectImpl e Just Nothing
 
 getVideoElementById
   :: forall e. String -> Eff ( video :: VIDEO | e ) (Maybe VideoElement)
