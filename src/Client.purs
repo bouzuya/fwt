25bb98446183767dddd85aed36489c5a55ad0cf1
bouzuya <m@bouzuya.net>
2018-05-04 16:58:19 +0900

    remove log

--- src/Client.purs
+++ src/Client.purs
@@ -6,7 +6,7 @@ import Component.App (app)
 import Control.Bind (bind, pure)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.AVar (AVAR)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Eff.Exception (EXCEPTION, error)
 import Control.Monad.Eff.Ref (REF)
 import Control.Monad.Eff.Timer (TIMER)
@@ -16,11 +16,9 @@ import DOM.Node.ParentNode (QuerySelector(..))
 import Data.Maybe (maybe)
 import Data.Unit (Unit, unit)
 import Graphics.Canvas (CANVAS)
-import Halogen (liftEff)
 import Halogen.Aff (awaitLoad, runHalogenAff, selectElement)
 import Halogen.VDom.Driver (runUI)
 import Network.HTTP.Affjax (AJAX)
-import Prelude (discard, ($))
 import Video (MEDIA, VIDEO)
 
 main :: forall e. Eff ( ajax :: AJAX
@@ -37,8 +35,7 @@ main :: forall e. Eff ( ajax :: AJAX
                       )
                       Unit
 main = runHalogenAff do
-  liftEff $ log "Hello!"
-  awaitLoad
+  _ <- awaitLoad
   appMaybe <- selectElement (QuerySelector ".body")
   appElement <- maybe (throwError (error "Could not find .body")) pure appMaybe
   runUI app unit appElement
