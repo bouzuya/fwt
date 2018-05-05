eccbeccb52cb425e3b79c03d5f1e677c32b7689f
bouzuya <m@bouzuya.net>
2018-05-05 23:17:26 +0900

    remove unused instance

--- src/Data/UserStatus.purs
+++ src/Data/UserStatus.purs
@@ -2,29 +2,11 @@ module Data.UserStatus
   ( UserStatus(UserStatus)
   ) where
 
-import Control.Bind (bind, pure)
-import Data.Argonaut (class DecodeJson, class EncodeJson, decodeJson, encodeJson, fromObject, jsonNull, (.?))
 import Data.FaceWithTime (FaceWithTime)
-import Data.Function (($))
-import Data.Maybe (Maybe, maybe)
-import Data.StrMap as StrMap
-import Data.Tuple (Tuple(..))
+import Data.Maybe (Maybe)
 import Data.User (User)
 
 newtype UserStatus = UserStatus
   { fwt :: Maybe FaceWithTime
   , user :: User
   }
-
-instance decodeJsonUserStatus :: DecodeJson UserStatus where
-  decodeJson json = do
-    o <- decodeJson json
-    user <- o .? "user"
-    fwt <- o .? "fwt"
-    pure $ UserStatus { user, fwt }
-
-instance encodeJsonUserStatus :: EncodeJson UserStatus where
-  encodeJson (UserStatus { user, fwt }) = fromObject $ StrMap.fromFoldable
-    [ Tuple "user" $ encodeJson user
-    , Tuple "fwt" $ maybe jsonNull encodeJson fwt
-    ]
