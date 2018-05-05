8bbab2881056bf8368e9f071e76a316b2ad17da0
bouzuya <m@bouzuya.net>
2018-05-05 23:18:55 +0900

    remove unused instance

--- src/Data/User.purs
+++ src/Data/User.purs
@@ -3,15 +3,8 @@ module Data.User
   , toClient
   ) where
 
-import Control.Applicative (pure)
-import Control.Bind (bind)
-import Data.Argonaut (class DecodeJson, class EncodeJson, decodeJson, encodeJson, fromObject, fromString, stringify, (.?))
 import Data.ClientUser (ClientUser(..))
 import Data.Eq (class Eq, eq)
-import Data.Function (($))
-import Data.Show (class Show)
-import Data.StrMap (fromFoldable) as StrMap
-import Data.Tuple (Tuple(..))
 import Data.UserId (UserId)
 
 newtype User =
@@ -21,26 +14,8 @@ newtype User =
   , password :: String
   }
 
-instance decodeJsonUser :: DecodeJson User where
-  decodeJson json = do
-    o <- decodeJson json
-    id <- o .? "id"
-    name <- o .? "name"
-    password <- o .? "password"
-    pure $ User { id, name, password }
-
-instance encodeJsonUser :: EncodeJson User where
-  encodeJson (User { id, name, password }) = fromObject $ StrMap.fromFoldable
-    [ Tuple "id" $ encodeJson id
-    , Tuple "name" $ fromString name
-    , Tuple "password" $ fromString password
-    ]
-
 instance eqUser :: Eq User where
   eq (User { id: a }) (User { id: b }) = eq a b
-
-instance showUser :: Show User where
-  show user = stringify $ encodeJson user
 
 toClient :: User -> ClientUser
 toClient (User { id, name }) =
